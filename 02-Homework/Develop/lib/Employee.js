5e78a9eb26f475491e84e77d00d1069616ca3e5b
GBondarek <georgettebondarek@gmail.com>
2020-12-13 13:10:49 -0500

    update roles

--- 02-Homework/Develop/lib/Employee.js
+++ 02-Homework/Develop/lib/Employee.js
@@ -1,7 +1,21 @@
 // TODO: Write code to define and export the Employee class
 class Employee{
-    constructor(email, name, id)
-    this.email = email
-    this.name = name;
-    this.id = id; 
+    constructor(email, name, id){
+        this.email = email;
+        this.name = name;
+        this.id = id; 
+    }
+   getEmail(){
+       return.this.email;
+   };
+   getName(){
+       return.this.name;
+   };
+   getID(){
+       return.this.id; 
+   };
+   getRole(){
+       return.this.role;
+   }
+   
 }
